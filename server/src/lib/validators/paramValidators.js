5858a502f7ae7053540a7c401d824d683a2a20f3
Jack Schofield <jjmschofield@googlemail.com>
2018-04-15 14:38:57 +0100

    Supporting number type ints and addig a validator for dates

--- server/src/lib/validators/paramValidators.js
+++ server/src/lib/validators/paramValidators.js
@@ -1,9 +1,27 @@
 const validator = require('validator');
+const dateFns = require('date-fns');
 
 const isValidInt = (int) => {
-  return typeof int === 'string' && validator.isInt(int) && parseInt(int, 10) < 2147483648;
+  if (typeof int === 'string') {
+    return validator.isInt(int)
+      && parseInt(int, 10) < 2147483648
+      && parseInt(int, 10) > -2147483648;
+  }
+
+  if (typeof int === 'number') {
+    return int < 2147483648
+      && int > -2147483648
+      && int.toString().indexOf('.') < 0;
+  }
+
+  return false;
+};
+
+const isValidDate = (date) => {
+  return dateFns.isValid(new Date(date));
 };
 
 module.exports = {
   isValidInt,
+  isValidDate,
 };
